9fe36672b99d46032ff02562741268f53d79afa6
Willian Bento <wbento2010@gmail.com>
2020-08-25 18:18:19 -0300

    First Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		Position pos = new Position(3,5);
+		System.out.println(pos);
 	}
 
 }
