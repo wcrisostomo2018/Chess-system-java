d8d0b6f1c40dd3ed84714b7b8105b89fde0be0b4
Willian Bento <wbento2010@gmail.com>
2020-08-26 20:33:21 -0300

    possible moves of a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -15,5 +15,12 @@ public class Rook extends ChessPiece{
 	public String toString() {
 		return "R";
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		// TODO Auto-generated method stub
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}	
 	
 }
