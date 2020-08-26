f92d254a1aa2ff6c41165ebd0d7f8ee99d3ad56f
Willian Bento <wbento2010@gmail.com>
2020-08-26 18:46:25 -0300

    Moving Pieces

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -46,6 +46,19 @@ public class Board {
 		
 	}
 	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.postition = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	private Boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
